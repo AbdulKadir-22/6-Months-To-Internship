55f2307bce9f2a13e04475713ceadca116efc7d4
Zeron-001 <yusuf22086@gmail.com>
2025-05-25 11:26:09 +0530

    Remove Day01-10 folder

--- Day01-10/Day01/areacalculator.py
+++
@@ -1,4 +0,0 @@
-length = float(input("Enter the length of the Rectangle: "))
-breadth = float(input("Enter the Breadth of the Rectangle: "))
-
-print(f"The Area of Rectangle is : {length * breadth}cm^2")
--- Day01-10/Day02/emailslicer.py
+++
@@ -1,8 +0,0 @@
-email = input("Enter Your Email: ")
-
-index = email.index("@")
-
-username = email[:index ]
-domain =  email[index + 1: ]
-
-print(f"Your Username is {username} and Domain is {domain}")
